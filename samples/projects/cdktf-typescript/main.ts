f98efff5a6bfd915b229220ab43f54ff85abcfd2
Chen Wang <dev@chenwang.org>
2022-11-28 12:43:20 +0000

    feat(packages)!: Fix wrong config in releasing RI package cdktf-typescript

--- samples/projects/cdktf-typescript/main.ts
+++ samples/projects/cdktf-typescript/main.ts
@@ -1,16 +1,15 @@
-// Copyright (c) HashiCorp, Inc
-// SPDX-License-Identifier: MPL-2.0
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    // define resources here
-  }
-}
+import { App } from "cdktf";
+import * as cdf from '@openfabr/cdf';
+import { PackageInfraConfig, PackagePlanner } from '@openfabr/package-ri-cdktf';
+import config from './lib/config.json';
 
 const app = new App();
-new MyStack(app, "testdrive-cdktf");
+
+cdf.cdktf.initProjectStack(
+  app,
+  config as PackageInfraConfig,
+  new PackagePlanner(),
+  [],
+);
+
 app.synth();
