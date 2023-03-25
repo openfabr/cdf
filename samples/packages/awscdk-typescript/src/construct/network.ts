ea7a104e770af3ae69f20a336ab44b7068787dbd
Janaka Abeywardhana <contact@janaka.co.uk>
2023-03-25 12:38:19 +0000

    refactor(package awscdk): net and services outputs
    add: outputs to services
    add: outputs to network

--- samples/packages/awscdk-typescript/src/construct/network.ts
+++ samples/packages/awscdk-typescript/src/construct/network.ts
@@ -1,32 +1,35 @@
-import { Vpc, IpAddresses, SubnetConfiguration } from "aws-cdk-lib/aws-ec2";
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
-import { NetworkSubnetDetail, NetworkSubnetType, PackageInfraConfig } from "../package-config";
+import { NetworkSubnetDetail, PackageInfraConfig } from "../package-config";
 
 export interface NetworkProps {
   config: PackageInfraConfig,
 }
 
 export class Network extends Construct {
-  readonly vpc: Vpc;
+  readonly vpc: ec2.Vpc;
 
   constructor(scope: Construct, id: string, props: NetworkProps) {
     super(scope, id);
 
-    const subnets = props.config.network.subnets?.map(d => <SubnetConfiguration>{
+    const subnets = props.config.network.subnets?.map(d => <ec2.SubnetConfiguration>{
       cidrMask: d.mask,
       subnetType: NetworkSubnetDetail.subnetType(d.type),
       name: d.name,
     });
 
-    const addresses = props.config.network.cidr ? IpAddresses.cidr(props.config.network.cidr) : undefined;
+    const addresses = props.config.network.cidr ? ec2.IpAddresses.cidr(props.config.network.cidr) : undefined;
     const vpcName = props.config.network.name;
 
     if (vpcName !== "default-vpc" && addresses) {
-      this.vpc = new Vpc(this, 'vpc', {
+      this.vpc = new ec2.Vpc(this, 'vpc', {
         vpcName: vpcName,
         subnetConfiguration: subnets,
         ... { ipAddresses: addresses },
       });
+      new cdk.CfnOutput(this.vpc, 'Vpc:Id', { value: this.vpc.vpcId, description: 'Id of the VPC.'});
+      new cdk.CfnOutput(this.vpc, 'Vpc:Arn', { value: this.vpc.vpcArn, description: 'ARN of the the VPC'});
     };
   }
 }
